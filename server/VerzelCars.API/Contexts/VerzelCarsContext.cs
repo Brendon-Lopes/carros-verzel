17652981d0a42a3fdcc0da2c74f7fada12564ac6
Brendon <brendon.lopes.21@gmail.com>
2023-03-06 03:01:22 -0300

    feat: add database Context

---
+++ server/VerzelCars.API/Contexts/VerzelCarsContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using VerzelCars.API.Models;
+
+namespace VerzelCars.API.Contexts;
+
+public class VerzelCarsContext : DbContext
+{
+    public VerzelCarsContext(DbContextOptions<VerzelCarsContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Car> Cars { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Brand> Brands { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().Property(u => u.CreatedAt).HasDefaultValueSql("GETDATE()");
+        modelBuilder.Entity<User>().Property(u => u.UpdatedAt).HasDefaultValueSql("GETDATE()");
+
+        modelBuilder.Entity<Car>().Property(c => c.CreatedAt).HasDefaultValueSql("GETDATE()");
+        modelBuilder.Entity<Car>().Property(c => c.UpdatedAt).HasDefaultValueSql("GETDATE()");
+
+        modelBuilder.Entity<Car>().Property(c => c.Price).HasColumnType("decimal(18,2)");
+    }
+}
