8cef7339ae4f055a467bbe0ce7cc528fb2183c1b
Brendon <brendon.lopes.21@gmail.com>
2023-03-06 03:02:47 -0300

    feat: add database Context to the builder service
    alongside with the connection string

--- server/VerzelCars.API/Program.cs
+++ server/VerzelCars.API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using VerzelCars.API.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<VerzelCarsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
