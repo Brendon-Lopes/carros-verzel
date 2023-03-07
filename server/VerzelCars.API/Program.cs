97d117e3dc133d508cd05faa3f3a8b02f6d21821
Brendon <brendon.lopes.21@gmail.com>
2023-03-06 21:56:13 -0300

    feat: add authentication config
    and dependency injections

--- server/VerzelCars.API/Program.cs
+++ server/VerzelCars.API/Program.cs
@@ -1,8 +1,13 @@
+using System.Text;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using VerzelCars.API.Configuration;
 using VerzelCars.API.Contexts;
-using VerzelCars.API.Contracts.Authentication;
+using VerzelCars.API.Repositories;
+using VerzelCars.API.Services.Authentication;
 using VerzelCars.API.Services.Validations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +20,11 @@ builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssemblyContaining<CreateUserValidation>();
 
+// Dependency Injection
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
+builder.Services.AddScoped<IPasswordHandler, PasswordHandler>();
+
 // Context
 builder.Services.AddDbContext<VerzelCarsContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -22,6 +32,32 @@ builder.Services.AddDbContext<VerzelCarsContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// JWT Authentication
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(jwt =>
+{
+    var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfig:Secret"]);
+
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false, // for dev
+        ValidateAudience = false, // for dev
+        RequireExpirationTime = false, // for dev
+        ValidateLifetime = true
+    };
+});
+
 
 var app = builder.Build();
 
@@ -34,6 +70,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
