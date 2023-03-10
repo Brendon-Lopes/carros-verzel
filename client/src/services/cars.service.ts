40d88028ca3fbf9f5d32620690d914fb1ef6c589
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 17:02:09 -0300

    feat: add all the dynamic parameters of the function
    that fetches the cars from the api

--- client/src/services/cars.service.ts
+++ client/src/services/cars.service.ts
@@ -1,12 +1,27 @@
 import { api } from '../providers/api'
 
-export const getAllCars = async () => {
+interface IOptions {
+  name?: string
+  order?: string
+  page?: number
+  pageSize?: number
+  brandName?: string
+}
+
+export const getAllCars = async ({
+  name = '',
+  order = 'desc',
+  brandName = '',
+  page = 1,
+  pageSize = 8,
+}: IOptions = {}) => {
+  let params: IOptions = { order, page, pageSize }
+
+  if (name !== '') params = { ...params, name }
+  if (brandName !== '') params = { ...params, brandName }
+
   try {
-    const { data } = await api.get('/cars', {
-      params: {
-        order: 'desc',
-      },
-    })
+    const { data } = await api.get('/cars', { params })
 
     return data
   } catch (err) {
