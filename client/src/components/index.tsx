dc12f68c4635fa56d64231930585bcc079d1f902
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 00:55:41 -0300

    chore: add import to index file

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,4 +1,5 @@
+import { CarCard } from './CarCard'
 import { NavBar } from './NavBar'
 import { SearchBar } from './SearchBar'
 
-export { NavBar, SearchBar }
+export { NavBar, SearchBar, CarCard }
