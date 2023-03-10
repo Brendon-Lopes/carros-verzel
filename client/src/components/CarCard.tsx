615c42e1416647d8f599416b28fb5e1cad1c5312
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 01:36:01 -0300

    style: fix card size variation problem

--- client/src/components/CarCard.tsx
+++ client/src/components/CarCard.tsx
@@ -6,16 +6,22 @@ interface IProps {
 
 export function CarCard({ car }: IProps) {
   return (
-    <div className='max-w-sm rounded overflow-hidden shadow-md m-auto'>
-      <img className='w-full' src={car.imageUrl} alt={car.name} />
-      <div className='px-6 py-4'>
-        <div className='font-bold text-xl mb-2'>{car.name}</div>
-        <p className='text-gray-700 text-base'>{`${car.brandName} - ${car.model} - Ano ${car.year}`}</p>
-      </div>
-      <div className='px-6 pt-4 pb-2'>
-        <span className='inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-          {car.price}
-        </span>
+    <div className='w-full p-4'>
+      <div className='h-full bg-white rounded-lg shadow-md'>
+        <img
+          className='w-full object-cover h-48 rounded-t-lg'
+          src={car.imageUrl}
+          alt={car.name}
+        />
+        <div className='p-4'>
+          <div className='font-bold text-xl mb-2'>{car.name}</div>
+          <p className='text-gray-700 text-base'>{`${car.brandName} - ${car.model} - Ano ${car.year}`}</p>
+          <div className='mt-4'>
+            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+              {car.price}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   )
