0cb7dfbd400c7be9921e0615ab08bf803964c14c
Brendon <brendon.lopes.21@gmail.com>
2023-03-09 23:42:07 -0300

    feat: add NavBar

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom'
+import { RiAdminLine } from 'react-icons/ri'
+
+export function NavBar() {
+  return (
+    <nav className='flex justify-center h-20 bg-slate-100 drop-shadow-md'>
+      <div className='w-full max-w-7xl flex justify-between items-center text-gray-700'>
+        <h1 className='hover:text-black transition-all'>Verzel Cars</h1>
+
+        <NavLink to={'/login'}>
+          <button
+            className='hover:text-black transition-all flex items-center gap-2 text-lg'
+            onClick={() => {
+              console.log('Login')
+            }}
+          >
+            <RiAdminLine />
+            Login
+          </button>
+        </NavLink>
+      </div>
+    </nav>
+  )
+}
