44aa2ecccef596f8f87721d7892c9def6f5aca00
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 16:16:45 -0300

    feat: add formated price to cards

--- client/src/components/CarCard.tsx
+++ client/src/components/CarCard.tsx
@@ -1,4 +1,5 @@
 import { type ICar } from '../interfaces'
+import { formatPrice } from '../utils'
 
 interface IProps {
   car: ICar
@@ -7,7 +8,7 @@ interface IProps {
 export function CarCard({ car }: IProps) {
   return (
     <div className='w-full p-4'>
-      <div className='h-full bg-white rounded-lg shadow-md'>
+      <div className='h-full bg-white rounded-lg shadow-md hover:shadow-lg hover:cursor-pointer'>
         <img
           className='w-full object-cover h-48 rounded-t-lg'
           src={car.imageUrl}
@@ -17,8 +18,8 @@ export function CarCard({ car }: IProps) {
           <div className='font-bold text-xl mb-2'>{car.name}</div>
           <p className='text-gray-700 text-base'>{`${car.brandName} - ${car.model} - Ano ${car.year}`}</p>
           <div className='mt-4'>
-            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-              {car.price}
+            <span className='inline-block text-2xl text-blue-600 mr-2 mb-2'>
+              {formatPrice(car.price)}
             </span>
           </div>
         </div>
