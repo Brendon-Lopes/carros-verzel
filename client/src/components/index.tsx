f855813561210706880ff1383adb2f05bb4410bd
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 18:37:40 -0300

    feat: :sparkles: add SideBar component
    containing filters

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,5 +1,6 @@
 import { CarCard } from './CarCard'
 import { NavBar } from './NavBar'
 import { SearchBar } from './SearchBar'
+import { SideBar } from './SideBar'
 
-export { NavBar, SearchBar, CarCard }
+export { NavBar, SearchBar, CarCard, SideBar }
