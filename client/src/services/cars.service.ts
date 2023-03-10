f52884635130a535dadeffb6ab026045d88d85f9
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 01:10:04 -0300

    feat: update car service get method params
    for order to be 'desc' for default

--- client/src/services/cars.service.ts
+++ client/src/services/cars.service.ts
@@ -2,7 +2,11 @@ import { api } from '../providers/api'
 
 export const getAllCars = async () => {
   try {
-    const { data } = await api.get('/cars')
+    const { data } = await api.get('/cars', {
+      params: {
+        order: 'desc',
+      },
+    })
 
     return data
   } catch (err) {
