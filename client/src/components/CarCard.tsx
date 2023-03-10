2be50baac5589abaecad09ceb250ba7d60ad82ee
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 01:11:00 -0300

    feat: update CarCard component

--- client/src/components/CarCard.tsx
+++ client/src/components/CarCard.tsx
@@ -6,13 +6,17 @@ interface IProps {
 
 export function CarCard({ car }: IProps) {
   return (
-    <main className=''>
-      <img src={car.imageUrl} alt={car.name} />
-      <h1>{car.name}</h1>
-      <p>{car.brandName}</p>
-      <p>{car.model}</p>
-      <p>{car.year}</p>
-      <h3>{car.price}</h3>
-    </main>
+    <div className='max-w-sm rounded overflow-hidden shadow-md m-auto'>
+      <img className='w-full' src={car.imageUrl} alt={car.name} />
+      <div className='px-6 py-4'>
+        <div className='font-bold text-xl mb-2'>{car.name}</div>
+        <p className='text-gray-700 text-base'>{`${car.brandName} - ${car.model} - Ano ${car.year}`}</p>
+      </div>
+      <div className='px-6 pt-4 pb-2'>
+        <span className='inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+          {car.price}
+        </span>
+      </div>
+    </div>
   )
 }
