d14c75a68b7990023fa742409ed660812b80780a
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 17:02:58 -0300

    feat: add function to filter the cars by name/model
    using the searchbar

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,14 +1,35 @@
+import { useState } from 'react'
 import { BsSearch } from 'react-icons/bs'
 
-export function SearchBar() {
+interface IProps {
+  onSearch: (search: string) => void
+}
+
+export function SearchBar({ onSearch }: IProps) {
+  const [searchValue, setSearchValue] = useState<string>('')
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value)
+  }
+
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    onSearch(searchValue)
+  }
+
   return (
-    <div className='bg-blue-600 h-16 flex justify-center items-center px-4'>
+    <div className='bg-blue-600 h-16 flex justify-center items-center px-4 mb-6'>
       <div className='relative max-w-7xl h-4/6 w-full flex items-center'>
-        <input
-          className='rounded-sm p-2 w-full h-full'
-          type='text'
-          placeholder='Buscar por marca ou modelo...'
-        />
+        <form onSubmit={onFormSubmit} className='w-full h-full'>
+          <input
+            className='rounded-sm p-2 w-full h-full'
+            type='text'
+            placeholder='Buscar por marca ou modelo...'
+            value={searchValue}
+            onChange={onInputChange}
+          />
+        </form>
+
         <BsSearch className='absolute right-2 text-xl text-gray-600' />
       </div>
     </div>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,6 +6,12 @@ import * as carsService from '../services/cars.service'
 export function Home() {
   const [cars, setCars] = useState<ICar[]>([])
 
+  const onSearch = (search: string) => {
+    void carsService.getAllCars({ name: search }).then((response) => {
+      setCars(response.cars)
+    })
+  }
+
   useEffect(() => {
     void carsService.getAllCars().then((response) => {
       setCars(response.cars)
@@ -15,9 +21,9 @@ export function Home() {
   return (
     <div>
       <NavBar />
-      <SearchBar />
+      <SearchBar onSearch={onSearch} />
 
-      <main className='max-w-7xl m-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+      <main className='max-w-7xl m-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-8'>
         {cars.length > 0 &&
           cars.map((car) => <CarCard key={car.carId} car={car} />)}
       </main>
