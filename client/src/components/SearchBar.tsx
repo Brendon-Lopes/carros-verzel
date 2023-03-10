0777ae783dc5b4918493da33aea3967390bd101e
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 00:56:29 -0300

    feat: update SearchBar component
    add placeholder and icon to text input

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,7 +1,16 @@
+import { BsSearch } from 'react-icons/bs'
+
 export function SearchBar() {
   return (
     <div className='bg-blue-600 h-16 flex justify-center items-center'>
-      <input className='max-w-7xl w-full h-4/6 rounded-sm p-2' type='text' />
+      <div className='relative max-w-7xl h-4/6 w-full flex items-center'>
+        <input
+          className='rounded-sm p-2 w-full h-full'
+          type='text'
+          placeholder='Buscar por marca ou modelo...'
+        />
+        <BsSearch className='absolute right-2 text-xl text-gray-600' />
+      </div>
     </div>
   )
 }
