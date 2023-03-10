a2570ed3dcd238495da60efc02a617e6921f47f0
Brendon <brendon.lopes.21@gmail.com>
2023-03-09 23:42:20 -0300

    feat: add SearchBar component

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,7 @@
+export function SearchBar() {
+  return (
+    <div className='bg-blue-600 h-16 flex justify-center items-center'>
+      <input className='max-w-7xl w-full h-4/6 rounded-sm p-2' type='text' />
+    </div>
+  )
+}
---
+++ client/src/components/index.tsx
@@ -0,0 +1,4 @@
+import { NavBar } from './NavBar'
+import { SearchBar } from './SearchBar'
+
+export { NavBar, SearchBar }
