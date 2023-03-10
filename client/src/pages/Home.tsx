f63d37853d8e7819ab8122475de1ef28e606e1c5
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 00:54:31 -0300

    feat: add api connection to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,25 @@
-import { NavBar, SearchBar } from '../components'
+import { useState, useEffect } from 'react'
+import { CarCard, NavBar, SearchBar } from '../components'
+import { type ICar } from '../interfaces'
+import * as carsService from '../services/cars.service'
 
 export function Home() {
+  const [cars, setCars] = useState<ICar[]>([])
+
+  useEffect(() => {
+    void carsService.getAllCars().then((response) => {
+      setCars(response.cars)
+    })
+  }, [])
+
   return (
     <div>
       <NavBar />
       <SearchBar />
 
-      <main>Home</main>
+      <main className='max-w-7xl m-auto bg-red-700'></main>
+      {cars.length > 0 &&
+        cars.map((car) => <CarCard key={car.carId} car={car} />)}
     </div>
   )
 }
