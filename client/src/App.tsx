a7d6f572208e661f322a76588947d9707ad0d0b7
Brendon <brendon.lopes.21@gmail.com>
2023-03-09 23:40:50 -0300

    feat: add router to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
+import { AppRoutes } from './routes'
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <AppRoutes />
 }
