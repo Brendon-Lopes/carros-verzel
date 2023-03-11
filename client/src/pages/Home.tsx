e153514bef9f1b303975bd267468c092a959ecdf
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 23:47:23 -0300

    feat: create the onCarDelete function
    to pass it to each car card as a prop

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { type IBrand, type ICar } from '../interfaces'
 import * as carsService from '../services/cars.service'
 import * as brandsService from '../services/brands.service'
 import { BsChevronRight, BsChevronLeft } from 'react-icons/bs'
+import { useCookies } from 'react-cookie'
 
 export function Home() {
   const [cars, setCars] = useState<ICar[]>([])
@@ -11,6 +12,8 @@ export function Home() {
   const [currentPage, setCurrentPage] = useState<number>(1)
   const [totalPages, setTotalPages] = useState<number>(1)
   const [selectedBrand, setSelectedBrand] = useState('')
+
+  const [cookies] = useCookies(['token', 'role'])
 
   const onSearch = (search: string) => {
     void carsService.getAllCars({ name: search }).then((response) => {
@@ -44,6 +47,34 @@ export function Home() {
 
       window.scrollTo(0, 0)
     }
+  }
+
+  const onCarDelete = (carId: string) => {
+    const token = cookies.token
+
+    if (cookies.role !== 'admin') {
+      alert('Você não tem permissão para deletar um carro')
+      return
+    }
+
+    void carsService
+      .deleteCar(carId, token)
+      .then((res) => {
+        console.log(res)
+
+        if (res === false) {
+          alert('Não foi possível deletar o carro')
+        }
+      })
+      .then(() => {
+        alert('Carro deletado com sucesso')
+
+        void carsService.getAllCars().then((response) => {
+          setCars(response.cars)
+          setCurrentPage(response.currentPage)
+          setTotalPages(response.totalPages)
+        })
+      })
   }
 
   useEffect(() => {
@@ -90,7 +121,9 @@ export function Home() {
         <section>
           <section className='grid grid-cols-2 md:grid-cols-3 mb-8'>
             {cars.length > 0 &&
-              cars.map((car) => <CarCard key={car.carId} car={car} />)}
+              cars.map((car) => (
+                <CarCard key={car.carId} car={car} onCarDelete={onCarDelete} />
+              ))}
           </section>
 
           <div className='w-full flex mb-12'>
