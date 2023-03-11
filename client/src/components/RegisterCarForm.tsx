08c8b8960e7a0b53b3ce74a3df400b89827ab47f
Brendon <brendon.lopes.21@gmail.com>
2023-03-11 01:17:04 -0300

    feat: refactor RegisterCardForm to be dynamic
    depending if user is creating or editing an existing car from the database

--- client/src/components/RegisterCarForm.tsx
+++ client/src/components/RegisterCarForm.tsx
@@ -4,13 +4,17 @@ import { type IBrand } from '../interfaces'
 import * as brandService from '../services/brands.service'
 import * as carService from '../services/cars.service'
 import { useCookies } from 'react-cookie'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import {
   createCarResolver,
   type ICreateCarFormData,
 } from '../validations/create-car.validation'
 
 export function RegisterCarForm() {
+  const location = useLocation()
+  const { state } = location
+  const editMode = state.editMode as boolean
+
   const [brands, setBrands] = useState<IBrand[]>([])
 
   const [cookies] = useCookies(['token'])
@@ -25,10 +29,12 @@ export function RegisterCarForm() {
     formState: { errors },
     register,
     handleSubmit,
+    setValue,
   } = formMethods
 
   const onSubmit = (data: ICreateCarFormData) => {
     const token = cookies.token
+
     void carService.createCar(data, token).then((res) => {
       if (res === false) {
         alert('Erro ao cadastrar!')
@@ -40,15 +46,37 @@ export function RegisterCarForm() {
     })
   }
 
+  const onEdit = (data: ICreateCarFormData) => {
+    console.log('edit')
+    const token = cookies.token
+
+    void carService.editCar(state.car!.carId, data, token).then((res) => {
+      if (res === false) {
+        alert('Erro ao editar!')
+        return
+      }
+
+      alert('Carro editado com sucesso!')
+      navigate('/')
+    })
+  }
+
   useEffect(() => {
     void brandService.getAllBrands().then((res) => {
       setBrands(res)
     })
-  }, [])
+
+    setValue('name', editMode ? state.car.name : '')
+    setValue('model', editMode ? state.car.model : '')
+    setValue('year', editMode ? state.car.year : 0)
+    setValue('price', editMode ? state.car.price : 0)
+    setValue('imageUrl', editMode ? state.car.imageUrl : '')
+    setValue('brand', editMode ? state.car.brandName : '')
+  }, [editMode])
 
   return (
     <form
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(editMode ? onEdit : onSubmit)}
       className='w-full sm:w-full md:w-3/4 lg:w-1/2 m-auto mt-6'
     >
       <div className='mb-6'>
@@ -85,32 +113,33 @@ export function RegisterCarForm() {
         )}
       </div>
 
-      <div className='mb-6'>
-        <label
-          htmlFor='brand'
-          className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-        >
-          Marca
-        </label>
-        <select
-          {...register('brand')}
-          id='brand'
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-          defaultValue={''}
-        >
-          <option disabled value=''>
-            Selecione uma marca
-          </option>
-          {brands.map((brand) => (
-            <option key={brand.brandId} value={brand.brandId}>
-              {brand.name}
+      {!editMode && (
+        <div className='mb-6'>
+          <label
+            htmlFor='brand'
+            className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+          >
+            Marca
+          </label>
+          <select
+            {...register('brand')}
+            defaultValue=''
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+          >
+            <option disabled value=''>
+              Selecione uma marca
             </option>
-          ))}
-        </select>
-        {errors.brand !== undefined && (
-          <p className='error-message'>{errors.brand.message}</p>
-        )}
-      </div>
+            {brands.map((brand) => (
+              <option key={brand.brandId} value={brand.brandId}>
+                {brand.name}
+              </option>
+            ))}
+          </select>
+          {errors.brand !== undefined && (
+            <p className='error-message'>{errors.brand.message}</p>
+          )}
+        </div>
+      )}
 
       <div className='mb-6'>
         <label
@@ -167,7 +196,7 @@ export function RegisterCarForm() {
         type='submit'
         className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'
       >
-        Adicionar
+        {editMode ? 'Editar' : 'Cadastrar'}
       </button>
     </form>
   )
