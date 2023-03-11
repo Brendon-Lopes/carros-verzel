0bd27159df566c665a2f1c4b3d0d27b1e89aed5a
Brendon <brendon.lopes.21@gmail.com>
2023-03-10 23:46:58 -0300

    feat: add option to delete a car on each card

--- client/src/components/CarCard.tsx
+++ client/src/components/CarCard.tsx
@@ -1,13 +1,27 @@
 import { type ICar } from '../interfaces'
 import { formatPrice } from '../utils'
+import { useCookies } from 'react-cookie'
+import { BsFillTrashFill } from 'react-icons/bs'
 
 interface IProps {
   car: ICar
+  onCarDelete: (carId: string) => void
 }
 
-export function CarCard({ car }: IProps) {
+export function CarCard({ car, onCarDelete }: IProps) {
+  const [cookies] = useCookies(['token', 'role'])
+
   return (
-    <div className='w-full p-4'>
+    <div className='w-full p-4 relative'>
+      {cookies.role === 'admin' && (
+        <BsFillTrashFill
+          onClick={() => {
+            onCarDelete(car.carId)
+          }}
+          className='absolute text-2xl text-red-600 bottom-9 right-9 hover:cursor-pointer hover:scale-125 transition-all'
+        />
+      )}
+
       <div className='h-full bg-white rounded-lg shadow-md hover:shadow-lg hover:cursor-pointer'>
         <img
           className='w-full object-cover h-48 rounded-t-lg'
